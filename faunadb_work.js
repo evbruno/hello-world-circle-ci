3aa836ebb239209d0a0ae9a8f6685dfb6829ad30
Eduardo V. Bruno <eduardo.bruno@fauna.com>
2020-04-20 17:46:21 -0300

    add extra tests and graphql

--- faunadb_work.js
+++ faunadb_work.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const faunadb = require('faunadb');
 const q = faunadb.query;
 
@@ -8,15 +10,22 @@ const client = new faunadb.Client({
   scheme: 'http' 
 });
 
-const coll_name = `my_coll_${Math.floor(Math.random() * 1024)}`
+function random_string(prefix) {
+  return `${prefix || 'my_coll'}_${Math.floor(Math.random() * 1024)}`
+}
 
-async function faunadb_work() {
+async function faunadb_work(size, coll_name) {
+  size = size || 256
+  coll_name = coll_name || random_string()
+
+  console.log(`size = ${size} coll_name = ${coll_name}`)
+
   await client.query(q.CreateCollection({name: coll_name}));
   
-  for(i = 0; i < 256; i++) {
+  for(i = 0; i < size; i++) {
     var exprs = [];
 
-    for(j = 0; j < 256; j++)
+    for(j = 0; j < size; j++)
       exprs.push(
         q.Create(
           q.Collection(coll_name), 
@@ -30,6 +39,52 @@ async function faunadb_work() {
   return await client.query(q.Count(q.Documents(q.Collection(coll_name))));
 }
 
+async function graphql_ping() {
+  return await fetch('http://localhost:8084/ping', { method: 'GET' }).then(c => c.text())
+}
+
+async function graphql_work() {
+  const db_name = random_string('db')
+  await client.query(q.CreateDatabase({name: db_name}))
+  const secret = await client.query(
+    q.Select(
+      'secret', 
+      q.CreateKey({role: 'server', database: q.Database(db_name) })
+  ));
+
+  console.log(`secret: ${secret} db: ${db_name}`)
+
+  await _run_graphql('import', 
+                      secret, 
+                      'type Todo { title: String! }, type Query { allTodos: [Todo!] }')
+  
+  do {
+    active = await client.query(q.Select("active", q.Get(q.Index("allTodos", q.Database(db_name)))))
+  } while(!active)
+
+  await _run_graphql('graphql', secret, '{"query": "mutation {createTodo(data: {title: \\"Howdy\\"}) { _id }}"}')
+  await _run_graphql('graphql', secret, '{"query": "mutation {createTodo(data: {title: \\"Partner\\"}) { _id }}"}')
+  
+  return await 
+    _run_graphql('graphql', secret, '{"query": "query {allTodos { data { title }}}"}')
+    .then(r => JSON.parse(r))
+}
+
+async function _run_graphql(endpoint, secret, payload) {
+  return await fetch(
+    `http://localhost:8084/${endpoint}`, 
+    { 
+      method: 'POST' ,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${secret}`
+      }, 
+      body: payload
+  }).then(c => c.text())
+}
+
+
 async function faunadb_setup() {
   console.log('setup...');
 }
@@ -38,4 +93,11 @@ async function faunadb_teardown() {
   console.log('teardown...');
 }
 
-module.exports = { faunadb_work, faunadb_setup, faunadb_teardown };
+module.exports = { 
+  faunadb_setup, 
+  faunadb_teardown,
+  faunadb_work, 
+  graphql_ping, 
+  graphql_work,
+  random_string
+};
--- faunadb_work.test.js
+++ faunadb_work.test.js
@@ -1,14 +1,36 @@
-const { faunadb_work, faunadb_setup, faunadb_teardown } = require('./faunadb_work');
+const ft = require('./faunadb_work');
 
 beforeAll(() => {
-  return faunadb_setup();
+  return ft.faunadb_setup();
 });
 
 afterAll(() => {
-  return faunadb_teardown();
+  return ft.faunadb_teardown();
 });
 
-test('can count the new entries', async () => {
-  const resp = await faunadb_work();
-  expect(resp).toBe(65536); 
+test('can count 256^2 new entries', async () => {
+  const resp = await ft.faunadb_work(256);
+    expect(resp).toBe(65536); 
+}, 300000);
+
+test('can count 10k new entries', async () => {
+  const resp = await ft.faunadb_work(100);
+  expect(resp).toBe(10000); 
+}, 300000);
+
+test('can count 40k new entries', async () => {
+  const resp = await ft.faunadb_work(200);
+  expect(resp).toBe(40000); 
+}, 300000);
+
+test('can access /ping graphql', async () => {
+  const resp = await ft.graphql_ping();
+  expect(resp).toBe('Up and running!'); 
+});
+
+test('can import/query graphql', async () => {
+  const resp = await ft.graphql_work();
+  expect(resp.data.allTodos.data.length).toBe(2); 
+  expect(resp.data.allTodos.data[0].title).toBe('Howdy'); 
+  expect(resp.data.allTodos.data[1].title).toBe('Partner'); 
 }, 300000);
